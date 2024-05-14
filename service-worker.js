9bcc4f13d656da284267ba354df47f29a2b842d2
AgonyLecter <142232640+AgonyLecter@users.noreply.github.com>
2024-05-14 21:05:56 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,63 @@
+// service-worker.js
+
+const CACHE_NAME = 'goelectric-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/reservations.html',
+  '/logo.png',
+  '/logo.ico',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css',
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css',
+  'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js',
+  'https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js',
+  'https://www.gstatic.com/firebasejs/8.0.0/firebase-database.js',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
+  'https://cdn.jsdelivr.net/npm/flatpickr',
+  'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js',
+  'https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+  
